2d04d6dc23ac23e6bbee547e2c69f983744b28a7
Anthony Hill <hillan1152@gmail.com>
2020-06-01 19:30:46 -0400

    quick refresher course

---
+++ components/SideMenu.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function SideMenu(props) {
+    console.log("props", props)
+    return (
+        <div>
+            <button onClick={props.clickHandler}>Click Me!</button>
+            <h1 className="my-4">{props.appName}</h1>
+            <div className="list-group">
+            <a href="#" className="list-group-item">Category 1</a>
+            <a href="#" className="list-group-item">Category 2</a>
+            <a href="#" className="list-group-item">Category 3</a>
+            </div>
+            <div>
+                <h1>{props.whatever}</h1>
+            </div>
+        </div>
+    )
+}
